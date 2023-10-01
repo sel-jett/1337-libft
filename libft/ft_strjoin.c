1298ea38e5366211b133fe544833f65ce6c3f7d3
salah <eljettiouisalaheddine@gmail.com>
2023-10-01 23:42:47 +0100

    need to protect substr

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -1,26 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
+#include "libft.h"
 char *ft_strjoin(char const *s1, char const *s2)
 {
-    int     len;
-    char    *str;
-    int     i;
-    int     j;
+    int len;
+    char *str;
+    int i;
+    int j;
 
-    if (!s1 && !s2)
+    if (!s1 || !s2)
         return (0);
     len = strlen(s1) + strlen(s2);
     str = malloc(sizeof(char) * len + 1);
+    if (!str)
+        return (0);
     i = 0;
     j = 0;
-    while(s1[i])
+    while (s1[i])
     {
         str[i] = s1[i];
         i++;
     }
-    while(s2[j])
+    while (s2[j])
     {
         str[i] = s2[j];
         i++;
@@ -29,11 +28,11 @@ char *ft_strjoin(char const *s1, char const *s2)
     str[i] = '\0';
     return (str);
 }
-
+/*
 int main()
 {
     char const *s1 = "";
     char const *s2 = "";
     printf("%s\n", ft_strjoin(s1, s2));
     return (0);
-}
+} */
--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -1,40 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "libft.h"
 
 char *ft_substr(char const *s, unsigned int start, size_t len)
 {
-    size_t  i;
-    char    *ptr;
-    int     check;
+    size_t i;
+    size_t size;
+    char *str;
 
+    size = ft_strlen(s);
     i = 0;
-    check = 0;
-    int thg = strlen(s) - i;
-    char const s1 = start;
-    while(s[i] != s1 && i < len)
-    {
-        check = 1;
-        i++;
-    }
-    if (i >= len)
+    if (!s)
         return (0);
-    ptr = malloc(sizeof(char) * thg + 1);
-    if (!ptr)
-        return (0);
-    int j = 0;
-    while (check == 1 && i < len)
+    if (start > size)
     {
-        ptr[j] = s[i];
-        j++;
-        i++;
+        str = ft_calloc(1, 1);
+        return (str);
     }
-    return ptr;
+    if (len > size)
+        len = size - start;
+    str = malloc(sizeof(char) * len + 1);
+    if (!str)
+        return (0);
+    int futstart = start;
+    while (s[futstart] && i < len && start < size)
+    {
+        str[i] = s[futstart];
+        i++;
+        futstart++;
+    }
+    str[i] = '\0';
+    return str;
 }
 
-int main()
+/* int main()
 {
     const char *s = "hello world";
-    printf("%s\n", ft_substr(s, 119, 20));
+    printf("%s\n", ft_substr(s, 7, 20));
     return (0);
-}
+} */

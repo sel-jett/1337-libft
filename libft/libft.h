5f00132ec4001c7dc0d83de1643e3ff54f4e04b1
salah <eljettiouisalaheddine@gmail.com>
2023-10-02 21:14:27 +0100

    ft_putchar_fd.v

--- libft/libft.h
+++ libft/libft.h
@@ -36,4 +36,5 @@ char *ft_strtrim(char const *s1, char const *set);
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void ft_striteri(char *s, void (*f)(unsigned int, char *));
 char **ft_split(char const *s, char c);
+void ft_putchar_fd(char c, int fd);
 #endif
